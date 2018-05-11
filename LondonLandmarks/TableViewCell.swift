4ce18d017e07edc36f8df2d538c2774e6b459a01
ViXette <v.xette@gmail.com>
2018-05-12 09:40:45 +1000

    Initial commit

---
+++ LondonLandmarks/TableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  Created by ViXette on 12/05/2018.
+//
+
+import UIKit
+
+
+class TableViewCell: UITableViewCell {
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		// Initialization code
+	}
+	
+	
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+		
+		// Configure the view for the selected state
+	}
+	
+}
